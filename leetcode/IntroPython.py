cbb8e1983a99eed2127a613f7421575cf7532269
brcheong <brcheong@tesla.cs.iupui.edu>
2022-02-03 11:50:44 -0500

    Updated IntroPython

--- leetcode/IntroPython.py
+++ leetcode/IntroPython.py
@@ -3,7 +3,7 @@ Introduction to Python
 This program takes input from the user and does data type conversions
 """
 name = raw_input('What is your name? ')
-print("Hello " + name)
+print("Hello " + name + ". Nice to meet you!")
 
 age = input ('How old are you? (Please enter an integer): ')
 
@@ -13,5 +13,7 @@ difference = str (difference)
 
 if age > my_age:
 	print("That's cool! You are " + difference + " years older than me!")
+elif my_age == age:
+	print("Hey what a coincidence, I am as old as you!")
 else: 
 	print("That's cool! You are " + difference + " years younger than me!")
